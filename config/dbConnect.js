fd413fcaf831a548638ee30b4249704f2397c4e8
ichchha-iagon <159746767+ichchharam-iagon@users.noreply.github.com>
2024-05-07 12:15:06 +0545

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connection = async () => {
   try {
-    console.log(process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
